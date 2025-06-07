9e7d84c504e6ec56a3559e53a805d5c0ee292d67
Prathamesh Kurekar <146461949+Prathamesshh@users.noreply.github.com>
2025-06-07 14:11:49 +0530

    Create 2894. Divisible and Non-divisible Sums Difference.cpp

---
+++ 2894. Divisible and Non-divisible Sums Difference.cpp
@@ -0,0 +1,17 @@
+/*
+You are given positive integers n and m.
+
+Define two integers as follows:
+
+num1: The sum of all integers in the range [1, n] (both inclusive) that are not divisible by m.
+num2: The sum of all integers in the range [1, n] (both inclusive) that are divisible by m.
+Return the integer num1 - num2
+*/
+
+class Solution {
+public:
+    int differenceOfSums(int n, int m) {
+        int k = n / m;
+        return n * (n + 1) / 2 - k * (k + 1) * m;
+    }
+};
