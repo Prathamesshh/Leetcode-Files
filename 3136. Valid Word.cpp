64b574c7f7a528c221945bf8cb67426d1a3bd1e3
Prathamesshh <prathameshkurekar@gmail.com>
2025-07-15 23:16:36 +0530

    create 3136. Valid Word.cpp

---
+++ 3136. Valid Word.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string word) {
+        if(word.size()<3) return false;
+
+        bool vowel = false;
+        bool consonant = false;
+        for(auto c:word){
+            if(isalpha(c)){
+                c = tolower(c);
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+                    vowel = true;
+
+                else
+                    consonant = true; 
+            }
+            else if(!isdigit(c)){
+                return false;
+            }
+        }
+        return vowel && consonant;
+    }
+};
