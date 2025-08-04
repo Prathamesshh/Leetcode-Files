b1f1a959cb2ac4e78cb2c8fc1490fa62f3e28b4b
Prathamesshh <prathameshkurekar@gmail.com>
2025-08-04 18:23:33 +0530

    Create 904. Fruit Into Baskets.cpp

---
+++ 904. Fruit Into Baskets.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int n = fruits.size();
+
+        unordered_map<int, int> mp;
+
+        int i = 0;
+        int j = 0;
+
+        int ans = INT_MIN;
+        while(j < n) {
+            mp[fruits[j]]++;
+
+            if(mp.size() <= 2) {
+                ans = max(ans, j-i+1);
+            }
+            else { 
+                mp[fruits[i]]--;
+
+                if(mp[fruits[i]] == 0) {
+                    mp.erase(fruits[i]);
+                }
+
+                i++;
+            }
+
+            j++;
+        }
+
+        return ans;
+    }
+};
