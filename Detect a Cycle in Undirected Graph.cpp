02d6334fe950ab0db1b66b93833dd75136465781
Prathamesshh <prathameshkurekar@gmail.com>
2025-08-21 15:12:35 +0530

    Create Detect a Cycle in Undirected Graph.cpp

---
+++ Detect a Cycle in Undirected Graph.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+public:
+    bool detect(int src,vector<int> adj[],int vis[]){
+        vis[src] =1;
+        queue<pair<int,int>> q;
+        q.push({src,-1});
+        while(!q.empty()){
+            int node = q.front().first;
+            int parent = q.front().second;
+            q.pop();
+
+            for(auto adjnode : adj[node]){
+                if(!vis[adjnode]){
+                    vis[adjnode] =1;
+                    q.push({adjnode,node});
+                }
+                else if(parent != adjnode){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    bool isCycle(int V, vector<int> adj[]) {
+        int vis[V] = {0};
+        for(int i=0;i<V;i++){
+            if(!vis[i])
+               if(detect(i,adj,vis)) return true;
+        }
+        return false;
+    }
+};
