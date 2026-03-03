a3433f21e6ab7af3339e55ccc5496c27b871a246
Prathamesh Kurekar <146461949+Prathamesshh@users.noreply.github.com>
2026-03-03 18:06:39 +0530

    Create 1536. Minimum Swaps to Arrange a Binary Grid.cpp

---
+++ 1536. Minimum Swaps to Arrange a Binary Grid.cpp
@@ -0,0 +1,44 @@
+/*
+Given an n x n binary grid, in one step you can choose two adjacent rows of the grid and swap them.
+
+A grid is said to be valid if all the cells above the main diagonal are zeros.
+
+Return the minimum number of steps needed to make the grid valid, or -1 if the grid cannot be valid.
+
+The main diagonal of a grid is the diagonal that starts at cell (1, 1) and ends at cell (n, n).
+*/
+
+class Solution {
+public:
+    int minSwaps(vector<vector<int>>& grid) {
+        int n = grid.size();
+        vector<int> pos(n, -1);
+        for (int i = 0; i < n; ++i) {
+            for (int j = n - 1; j >= 0; --j) {
+                if (grid[i][j] == 1) {
+                    pos[i] = j;
+                    break;
+                }
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            int k = -1;
+            for (int j = i; j < n; ++j) {
+                if (pos[j] <= i) {
+                    ans += j - i;
+                    k = j;
+                    break;
+                }
+            }
+            if (~k) {
+                for (int j = k; j > i; --j) {
+                    swap(pos[j], pos[j - 1]);
+                }
+            } else {
+                return -1;
+            }
+        }
+        return ans;
+    }
+};
