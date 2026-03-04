7818f6aaf1c382db6d5b7f4dfd4c88914757edf3
Prathamesh Kurekar <146461949+Prathamesshh@users.noreply.github.com>
2026-03-04 23:28:46 +0530

    Create 1582. Special Positions in a Binary Matrix.cpp

---
+++ 1582. Special Positions in a Binary Matrix.cpp
@@ -0,0 +1,43 @@
+/*
+Given an m x n binary matrix mat, return the number of special positions in mat.
+
+A position (i, j) is called special if mat[i][j] == 1 and all other elements in row i and column j are 0 (rows and columns are 0-indexed).
+  */
+
+class Solution {
+public:
+    int numSpecial(vector<vector<int>>& mat) {
+        int ans = 0;
+        int m = mat.size();
+        int n = mat[0].size();
+        
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                if (mat[row][col] == 0) {
+                    continue;
+                }
+                
+                bool good = true;
+                for (int r = 0; r < m; r++) {
+                    if (r != row && mat[r][col] == 1) {
+                        good = false;
+                        break;
+                    }
+                }
+                
+                for (int c = 0; c < n; c++) {
+                    if (c != col && mat[row][c] == 1) {
+                        good = false;
+                        break;
+                    }
+                }
+                
+                if (good) {
+                    ans++;
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
