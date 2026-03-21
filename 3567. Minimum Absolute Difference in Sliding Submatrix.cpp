9b0299fac79d8dc95536cf2da4b7c40235e3bb33
Prathamesh Kurekar <146461949+Prathamesshh@users.noreply.github.com>
2026-03-21 17:36:55 +0530

    Create 3567. Minimum Absolute Difference in Sliding Submatrix.cpp

---
+++ 3567. Minimum Absolute Difference in Sliding Submatrix.cpp
@@ -0,0 +1,37 @@
+/*
+You are given an m x n integer matrix grid and an integer k.
+For every contiguous k x k submatrix of grid, compute the minimum absolute difference between any two distinct values within that submatrix.
+Return a 2D array ans of size (m - k + 1) x (n - k + 1), where ans[i][j] is the minimum absolute difference in the submatrix whose top-left corner is (i, j) in grid.
+Note: If all elements in the submatrix have the same value, the answer will be 0.
+A submatrix (x1, y1, x2, y2) is a matrix that is formed by choosing all cells matrix[x][y] where x1 <= x <= x2 and y1 <= y <= y2.
+*/
+
+class Solution {
+public:
+    vector<vector<int>> minAbsDiff(vector<vector<int>>& grid, int k) {
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<int>> res(m - k + 1, vector<int>(n - k + 1));
+        for (int i = 0; i + k <= m; i++) {
+            for (int j = 0; j + k <= n; j++) {
+                vector<int> kgrid;
+                for (int x = i; x < i + k; x++) {
+                    for (int y = j; y < j + k; y++) {
+                        kgrid.push_back(grid[x][y]);
+                    }
+                }
+                int kmin = INT_MAX;
+                sort(kgrid.begin(), kgrid.end());
+                for (int t = 1; t < kgrid.size(); t++) {
+                    if (kgrid[t] == kgrid[t - 1]) {
+                        continue;
+                    }
+                    kmin = min(kmin, kgrid[t] - kgrid[t - 1]);
+                }
+                if (kmin != INT_MAX) {
+                    res[i][j] = kmin;
+                }
+            }
+        }
+        return res;
+    }
+};
