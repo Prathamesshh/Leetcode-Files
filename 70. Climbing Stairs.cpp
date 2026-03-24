dc969920fca4bae168eed312899e053cc8eabeb8
Prathamesh Kurekar <146461949+Prathamesshh@users.noreply.github.com>
2026-03-24 15:44:58 +0530

    Optimize climbStairs function for space complexity
    Refactor climbStairs function to use O(1) space.

--- 70. Climbing Stairs.cpp
+++ 70. Climbing Stairs.cpp
@@ -1,13 +1,32 @@
+TC - O(N)  SC- O(N)
+// class Solution {
+// public:
+//     int climbStairs(int n) {
+//         if(n <= 2)return n;
+
+//         vector<int> dp(n + 1, 0);
+//         dp[1] = 1; dp[2] = 2;
+
+//         for(int i = 3; i <= n; i++)dp[i] = dp[i - 1] + dp[i - 2];
+
+//         return dp[n];
+//     }
+// };
+
+
+    TC - O(N)   SC-O(1)
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n <= 2)return n;
+        if(n==0 || n==1)
+        return n;
 
-        vector<int> dp(n + 1, 0);
-        dp[1] = 1; dp[2] = 2;
-
-        for(int i = 3; i <= n; i++)dp[i] = dp[i - 1] + dp[i - 2];
-
-        return dp[n];
+        int prev2=1, prev1=1 , curr;
+        for(int i=2;i<=n;i++){
+            curr = prev1+prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return curr;
     }
 };
