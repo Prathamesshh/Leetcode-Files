f2fedb33796cb5c3badd2d63ae3aa4d79f6ae2f3
Prathamesh Kurekar <146461949+Prathamesshh@users.noreply.github.com>
2026-04-18 22:46:46 +0530

    Create 3783. Mirror Distance of an Integer.cpp

---
+++ 3783. Mirror Distance of an Integer.cpp
@@ -0,0 +1,36 @@
+/*You are given an integer n.
+
+Define its mirror distance as: abs(n - reverse(n))​​​​​​​ where reverse(n) is the integer formed by reversing the digits of n.
+
+Return an integer denoting the mirror distance of n​​​​​​​.
+
+abs(x) denotes the absolute value of x.
+
+ 
+
+Example 1:
+
+Input: n = 25
+
+Output: 27
+
+Explanation:
+
+reverse(25) = 52.
+Thus, the answer is abs(25 - 52) = 27.
+  */
+
+
+class Solution {
+public:
+    int reverse(int n) {
+        int res = 0;
+        while (n > 0) {
+            res = res * 10 + n % 10;
+            n /= 10;
+        }
+        return res;
+    }
+
+    int mirrorDistance(int n) { return abs(n - reverse(n)); }
+};
