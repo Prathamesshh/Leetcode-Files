456b051ab54a2460ee857ed2312bfe9d026f6e61
Prathamesh Kurekar <146461949+Prathamesshh@users.noreply.github.com>
2026-08-16 17:48:27 +0530

    Time: 7 ms (39.59%), Space: 131.2 MB (69.29%) - LeetHub

--- 2029-stone-game-ix/2029-stone-game-ix.cpp
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     bool stoneGameIX(vector<int>& stones) {
-        int cnt0 = 0, cnt1 = 0, cnt2 = 0;
-        for (int val : stones) {
-            if (int type = val % 3; type == 0) {
-                ++cnt0;
-            } else if (type == 1) {
-                ++cnt1;
-            } else {
-                ++cnt2;
-            }
-        }
-        if (cnt0 % 2 == 0) {
-            return cnt1 >= 1 && cnt2 >= 1;
-        }
-        return cnt1 - cnt2 > 2 || cnt2 - cnt1 > 2;
+        int f[3] = {0, 0, 0};
+
+        for (auto& s : stones)
+            f[s % 3]++;
+
+        if (~f[0] & 1)
+            return min(f[1], f[2]) >= 1;
+
+        return abs(f[1] - f[2]) >= 3;
     }
 };
